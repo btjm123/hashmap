12217871eed4d55599a5b2016ac487b07c38af95
Benn Tan <btjm123@gmail.com>
2024-08-09 21:28:09 +0800

    rempve unnecessary imports

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,29 @@
 #include "ChainingHashMap.h"
 #include "OpenAddressingHashMap.h"
 
-#include <algorithm>
 int main()
 {
-    // benn::ChainingHashMap<int, int> yay;
-    // yay[0] = 10;
-    // std::cout << yay[0] << '\n';
+    benn::ChainingHashMap<int, int> yay;
+    yay[0] = 10;
+    std::cout << yay[0] << '\n';
 
-    // yay[1] = 100;
-    // std::cout << yay[1] << '\n';
+    yay[1] = 100;
+    std::cout << yay[1] << '\n';
 
-    // yay.insert(std::pair<int, int>{2, 1000});
-    // std::cout << yay[2] << '\n';
+    yay.insert(std::pair<int, int>{2, 1000});
+    std::cout << yay[2] << '\n';
 
-    // yay.insert(std::pair<int, int>{1, 69});
-    // std::cout << yay[1] << '\n';
+    yay.insert(std::pair<int, int>{1, 69});
+    std::cout << yay[1] << '\n';
 
-    // yay.remove(5);
-    // std::cout << yay.size() << '\n';
-    // yay.remove(1);
-    // std::cout << yay.size() << '\n';
-    // std::cout << yay[1] << '\n';
-    // std::cout << yay.size() << '\n';
+    yay.remove(5);
+    std::cout << yay.size() << '\n';
+    yay.remove(1);
+    std::cout << yay.size() << '\n';
+    std::cout << yay[1] << '\n';
+    std::cout << yay.size() << '\n';
+
+    std::cout << "\n\n\n\n";
 
     benn::OpenAddressingHashMap<int, int> yay2;
     yay2[0] = 1;
